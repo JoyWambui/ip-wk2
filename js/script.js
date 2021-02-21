1215a3dc8080f7f4cd4f38a172f02efe8cdb6e69
JoyWambui <JoyWambui@github.com>
2021-02-21 21:10:18 +0300

    add radio buttons to choose gender.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-function akanName(){
-  let day = parseInt(document.getElementById("day").value);
-  if(document.getElementById("day") === ""){
-    alert("Please input day!");
-  } else if (document.getElementById("day").value <= 0 || document.getElementById("day").value >31){
-    document.getElementById("daydemo").innerHTML = "Out of range!"
+const akanName = () => {
+  if(document.getElementsByName("gender").checked === false){
+    alert("Please choose your gender");
     return false;
   }
   else{
     return true;
   }
 }
-akanName();
+
 
 
