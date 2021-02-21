79714d116d3b8b2d90c05e019bb1b63d4cde3d21
JoyWambui <JoyWambui@github.com>
2021-02-21 23:33:27 +0300

    add akan names array and radio buttons checked variable.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@
   var yearNumber = y%100;
   var centuryNumber = parseInt(y/100);
   let result = parseInt((d + ((13*(m + 1))/5) + yearNumber + (yearNumber/4) + (centuryNumber/4) + (5*centuryNumber))%7)
-  alert(result);
+  var akanMale =[Kwame,Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi]
+  var akanFemale= [Ama,Akosua,Adwoa,Abenaa,Akua,Yaa,Afua]
+  var selectMale= document.getElementById("male").checked;
+  var selectFemale= document.getElementById("female").checked;
 }
 
 
