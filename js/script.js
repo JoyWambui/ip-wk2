63986fd7bc81455701aa43a76eff1c7056ead427
JoyWambui <JoyWambui@github.com>
2021-02-21 20:20:27 +0300

    add additional instructions for format.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-var weekDay = function(number1,number2,number3,number4){
-  return result
-  };
-  var number1 = parseInt(prompt("Day of birth"));
-  var number2 = parseInt(prompt("Month of birth"));
-  var number3 = parseInt(prompt("Year of birth"))%100;
-  var number4 = parseInt(prompt("Year of birth"))/100;
-  let result = parseInt((number1 + ((13*(number2 + 1))/5) + number3 + (number3/4) + (number4/4) + (5*number4))%7)
-  weekDay(number1, number2, number3, number4);
-  alert("weekday = " + result);
+function akanName(){
+  let day = parseInt(document.getElementById("day").value);
+  if(document.getElementById("day") === ""){
+    alert("Please input day!");
+  } else if (document.getElementById("day").value <= 0 || document.getElementById("day").value >31){
+    document.getElementById("daydemo").innerHTML = "Out of range!"
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+akanName();
+
+
