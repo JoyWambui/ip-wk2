31026d45659005dfb7126675622731549af92617
JoyWambui <JoyWambui@github.com>
2021-02-22 08:54:51 +0300

    add javascript but it is not working.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,47 @@
   var akanFemale= [Ama,Akosua,Adwoa,Abenaa,Akua,Yaa,Afua]
   var selectMale= document.getElementById("male").checked;
   var selectFemale= document.getElementById("female").checked;
+    if (selectMale==true){
+      if(result===0){
+        alert("Your Akan name is " + akanMale[0]);    
+      }else if(result===1){
+        alert("Your Akan name is " + akanMale[1]);
+      }else if(result===2){
+        alert("Your Akan name is " + akanMale[2]);
+      }else if(result===3){
+        alert("Your Akan name is " + akanMale[3]);
+      }else if(result===4){
+        alert("Your Akan name is " + akanMale[4]);
+      }else if(result===5){
+        alert("Your Akan name is " + akanMale[5]);
+      }else if(result===6){
+        alert("Your Akan name is " + akanMale[6]);
+      }
+    }else  if (selectFemale==true)
+    {
+      if(result===0)
+      {
+        alert("Your Akan name is " + akanFemale[0]);
+      }   else if(result===1)
+      {
+        alert("Your Akan name is " + akanFemale[1]);
+      }   else if(result===2)
+      {
+        alert("Your Akan name is " + akanFemale[2]);
+      }   else if(result===3)
+      {
+        alert("Your Akan name is " + akanFemale[3]);
+      }   else if(result===4)
+      {
+        alert("Your Akan name is " + akanFemale[4]);
+      }   else if(result===5)
+      {
+        alert("Your Akan name is " + akanFemale[5]);
+      }   else if(result===6)
+      {
+        alert("Your Akan name is " + akanFemale[6]);
+    }
+ }
 }
 
-
+ 
