10015899344e558306b9af8336837871d13ab5c1
JoyWambui <JoyWambui@github.com>
2021-02-24 17:56:42 +0300

    add code that chooses name based on day previously got.

--- js/script.js
+++ js/script.js
@@ -9,60 +9,20 @@ window.addEventListener("DOMContentLoaded", function(event){
     console.log(newDate);
     let day = newDate.getDay();
     console.log(day);
-    let akanMale =["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+    let akanMale =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
       console.log("maleakan", akanMale);
-    let akanFemale= ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
+    let akanFemale= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
       console.log("femaleakan", akanFemale);
     let maleCheck= document.querySelector("#selectMale").checked;
       console.log("malecheck", maleCheck);
     let femaleCheck= document.querySelector("#selectFemale").checked;
       console.log("femalecheck", femaleCheck);
     if(maleCheck ===true){
-      if(weekDay === 0){
-        console.log("akanname",akanMale[0])
-        alert("your Akan name is: " + akanMale[0]);
-      }else if(weekDay === 1){
-        console.log("akanname",akanMale[1])
-        alert("your Akan name is: " + akanMale[1]);
-      }else if(weekDay === 2){
-        console.log("akanname",akanMale[2])
-        alert("your Akan name is: " + akanMale[2]);
-      }else if(weekDay === 3){
-        console.log("akanname",akanMale[3])
-        alert("your Akan name is: " + akanMale[3]);
-      }else if(weekDay === 4){
-        console.log("akanname",akanMale[4])
-        alert("your Akan name is: " + akanMale[4]);
-      }else if(weekDay === 5){
-        console.log("akanname",akanMale[5])
-        alert("your Akan name is: " + akanMale[5]);
-      }else if(weekDay === 6){
-        console.log("akanname",akanMale[6])
-        alert("your Akan name is: " + akanMale[6]);
-      }
+      console.log(akanMale[day]);
+      alert("Your Akan name is: " + akanMale[day]);
     }else if(femaleCheck === true){
-      if(weekDay === 0){
-        console.log("akanname",akanFemale[0])
-        alert("your Akan name is: " + akanFemale[0]);
-      }else if(weekDay === 1){
-        console.log("akanname",akanFemale[1])
-        alert("your Akan name is: " + akanFemale[1]);
-      }else if(weekDay === 2){
-        console.log("akanname",akanFemale[2])
-        alert("your Akan name is: " + akanFemale[2]);
-      }else if(weekDay === 3){
-        console.log("akanname",akanFemale[3])
-        alert("your Akan name is: " + akanFemale[3]);
-      }else if(weekDay === 4){
-        console.log("akanname",akanFemale[4])
-        alert("your Akan name is: " + akanFemale[4]);
-      }else if(weekDay === 5){
-        console.log("akanname",akanFemale[5])
-        alert("your Akan name is: " + akanFemale[5]);
-      }else if(weekDay === 6){
-        console.log("akanname",akanFemale[6])
-        alert("your Akan name is: " + akanFemale[6]);
-      }
+        console.log(akanFemale[day])
+        alert("your Akan name is: " + akanFemale[day]);
     }
   }
   let akanForm = document.querySelector("#akanForm");
